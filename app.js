e75ec922318ee0de44cae9e59985e7eece045d2e
Steph <stephane.manet.cm@gmail.com>
2017-11-19 23:29:59 +0100

    paramètres particles

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional));
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-*/
 particlesJS('particles-js',
 
 {
